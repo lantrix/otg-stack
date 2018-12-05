daa4ec5731d88ea5252d351cc1c6fa132dbb9ea8
Lantrix <lantrix@pobox.com>
2018-12-05 22:52:17 +1100

    Silent curl during provision

--- provision/otg-stack.sh
+++ provision/otg-stack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/lantrix/nginx-proxy/master/nginx.tmpl > /usr/local/nginx.tmpl
+curl -s https://raw.githubusercontent.com/lantrix/nginx-proxy/master/nginx.tmpl > /usr/local/nginx.tmpl
 docker-compose up -d
