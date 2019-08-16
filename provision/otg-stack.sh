335ae7d571810754701b574628d9ef4a3400bd6e
Ted B <lantrix@pobox.com>
2019-08-17 00:04:45 +1000

    Update otg-stack.sh
    Source env stuff

--- provision/otg-stack.sh
+++ provision/otg-stack.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+set -euxo pipefail
+cat .env >> ~/.bashrc
+. .env
 curl -s https://raw.githubusercontent.com/lantrix/nginx-proxy/master/nginx.tmpl > /usr/local/nginx.tmpl
 docker-compose up -d
